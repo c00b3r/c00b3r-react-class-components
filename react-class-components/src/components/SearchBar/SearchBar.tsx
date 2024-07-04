cacb049b93447f7ff12957e4adc4d26687f443f7
c00b3r <sanuyOKmaklaren0>
2024-07-04 22:13:41 +0500

    abaoa

--- react-class-components/src/components/SearchBar/SearchBar.tsx
+++ react-class-components/src/components/SearchBar/SearchBar.tsx
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class SearchBar extends Component {
   render() {
-    return <div>SearchBar</div>;
+    return <div>SearchBar</div><h1>special error for eslint and check husky</h1>;
   }
 }
