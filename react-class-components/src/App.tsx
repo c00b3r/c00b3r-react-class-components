f4f086d9112e7732522d4f063699f9642133710e
c00b3r <sanuyOKmaklaren0>
2024-07-04 23:49:07 +0500

    feat: implement fetch data from API

--- react-class-components/src/App.tsx
+++ react-class-components/src/App.tsx
@@ -2,13 +2,63 @@ import React, { Component } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ListOfPeople from "./components/ListOfPeople/ListOfPeople";
-// const response = await fetch("https://swapi.dev/api/people/?page=1");
+
+interface Data {
+  count: number;
+  next: string;
+  previous: string | null;
+  results: Results[];
+}
+
+interface Results {
+  birth_year: string;
+  created: string;
+  edited: string;
+  eye_color: string;
+  films: string[];
+  gender: string;
+  hair_color: string;
+  height: string;
+  homeworld: string;
+  mass: string;
+  name: string;
+  skin_color: string;
+  species: [];
+  starships: string[];
+  url: string;
+  vehicles: string[];
+}
+
+interface AppState {
+  data: Data;
+}
 export default class App extends Component {
+  state: Readonly<AppState> = {
+    data: {
+      count: 0,
+      next: "",
+      previous: null,
+      results: [],
+    },
+  };
+  componentDidMount(): void {
+    this.fetchDataOfPeople();
+  }
+
+  fetchDataOfPeople = async () => {
+    try {
+      const response = await fetch("https://swapi.dev/api/people/?page=1");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
   render() {
     return (
       <>
         <SearchBar />
-        <ListOfPeople />
+        <ListOfPeople result={this.state.data}/>
       </>
     );
   }
