79fb02446550d37a8097699b47291abcf396c854
c00b3r <sanuyOKmaklaren0>
2024-07-04 23:30:31 +0500

    feat: implement previously saved search term in the local storage into SearchBar component

--- react-class-components/src/components/SearchBar/SearchBar.tsx
+++ react-class-components/src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
+import "./styles.css";
 
 export default class SearchBar extends Component {
-  const aaaa = dddd;
+  state: Readonly<{ valueInput: string }> = {
+    valueInput: localStorage.getItem("prevSearchItem") ?? "",
+  };
+
+  constructor(props: string) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({ valueInput: event.target?.value });
+  }
+
+  handleClick() {
+    if (this.state.valueInput) {
+      localStorage.setItem("prevSearchItem", this.state.valueInput);
+    }
+    this.setState({ valueInput: "" });
+  }
+
   render() {
     return (
       <div>
-        <input type="text" />
-        <button>Seatch</button>
+        <input
+          type="text"
+          value={this.state.valueInput}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <button onClick={this.handleClick}>Seatch</button>
       </div>
     );
   }
