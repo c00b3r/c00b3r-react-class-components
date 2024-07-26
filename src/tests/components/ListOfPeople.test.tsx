e1b4a5816d0b66b942f9a6e7657c1874df0e7842
c00b3r <sanuyOKmaklaren0>
2024-07-26 21:06:13 +0500

    feat: add tests for ListOfPeople, LightDarkThemeContext, useLocalStorage components

--- src/tests/components/ListOfPeople.test.tsx
+++ src/tests/components/ListOfPeople.test.tsx
@@ -1,12 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import ListOfPeople from "../../components/ListOfPeople/ListOfPeople";
-import { BrowserRouter as Router } from "react-router-dom";
+import { MemoryRouter, BrowserRouter as Router } from "react-router-dom";
 import { mockData } from "../mockData";
 import { Provider } from "react-redux";
 import store from "../../store/store";
 
 describe("ListOfPeople", () => {
-  it("should render list of people correctly when data is available", () => {
+  it("should render list of people correctly when data is not available", () => {
     render(
       <Router>
         <Provider store={store}>
@@ -61,5 +61,45 @@ describe("ListOfPeople", () => {
 
     expect(screen.getByText("←")).toBeInTheDocument();
     expect(screen.getByText("→")).toBeInTheDocument();
+  });
+
+  it("calls set when pagination button is clicked", () => {
+    const setPage = vi.fn();
+    const { getByText } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ListOfPeople
+            data={{
+              count: 21,
+              next: "http://swapi.dev/api/people/?page=2",
+              previous: null,
+              results: [],
+            }}
+            page={2}
+            setPage={setPage}
+          />
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    const previousButton = getByText("←");
+    fireEvent.click(previousButton);
+
+    expect(setPage).toHaveBeenCalledWith(1);
+  });
+
+  it("calls handleCheckboxChange when checkbox is clicked", () => {
+    const { getAllByRole } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ListOfPeople data={mockData} page={1} setPage={() => {}} />
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    const checkbox = getAllByRole("checkbox");
+    fireEvent.click(checkbox[0]);
+
+    expect(checkbox[0]).toBeInTheDocument();
   });
 });
